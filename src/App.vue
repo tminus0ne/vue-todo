2f6838b68697b9ef3a9e4bd999f97fce78aa908b
Greg-One <sgmdoc@yandex.ru>
2021-08-12 22:43:08 +0500

    add post creation logic

---
+++ src/App.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="page">
+    <form @submit.prevent>
+      <h3>Create post</h3>
+      <input
+        v-bind:value="title"
+        @input="title = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="Title"
+      />
+      <input
+        v-bind:value="body"
+        @input="body = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="Description"
+      />
+      <button class="button" @click="createPost">Apply post</button>
+    </form>
+
+    <div class="post" v-for="post in posts">
+      <div><strong>Title:</strong> {{ post.title }}</div>
+      <div><strong>Description:</strong> {{ post.body }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: [
+        { id: 1, title: 'Post title', body: 'Post description' },
+        { id: 2, title: 'Post title', body: 'Post description' },
+        { id: 3, title: 'Post title', body: 'Post description' },
+        { id: 4, title: 'Post title', body: 'Post description' },
+      ],
+      title: '',
+      body: '',
+    };
+  },
+  methods: {
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      };
+      this.posts.push(newPost);
+      this.title = '';
+      this.body = '';
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.page {
+  padding: 20px;
+}
+
+.post {
+  padding: 15px;
+  margin-top: 15px;
+  border: 2px solid teal;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.button {
+  align-self: center;
+  padding: 10px 15px;
+  margin-top: 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
+
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+</style>
